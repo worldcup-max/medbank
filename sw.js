6460d7ffbc7ca0fd380bb45bb3c5071b52262887
Frank Jay <frankthejay@gmail.com>
2026-07-30 03:53:39 +0100

    new

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* MedBank service worker — offline caching + best-effort daily reminder */
-const CACHE = 'medbank-v32';
+const CACHE = 'medbank-v33';
 const ASSETS = ['./', './index.html', './content.js', './icon.svg', './manifest.webmanifest'];
 
 self.addEventListener('install', e => {
